6d417f453b40c00e26f43c91815f92802cb8438c
JustineAmolato <161929042+JustineAmolato@users.noreply.github.com>
2025-04-07 23:16:50 +0800

    Update settings.py

--- Badtodolist/settings.py
+++ Badtodolist/settings.py
@@ -158,7 +158,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  
     "https://justineamolato.github.io",
-     "http://localhost:8000",   
+     "http://localhost:8000",
+    "http://localhost:5174",
 ]
 
 
